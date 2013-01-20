15a23e693a951aca54f1f8780f81443d0f7b8ee6
jiikko <n905i.1214@gmail.com>
2013-01-20 18:08:52 +0900

    add app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeliveryStack::Application.config.secret_token = '394a1b3c85b2ed94f4463c5d709c561c94cd11cfdbd2d33980d722f9712608d63e51742d6adab3ee4e9bc07f3a5d1a84a11ca70092eb4ddfb5cd1b811f494ab9'
